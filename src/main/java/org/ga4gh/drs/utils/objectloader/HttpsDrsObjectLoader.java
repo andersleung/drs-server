de3c9fe7b58962c676c967b03171f63cfc01fc0c
Anders Leung <anders.leung@ga4gh.org>
2021-01-26 13:52:27 -0500

    WIP

--- src/main/java/org/ga4gh/drs/utils/objectloader/HttpsDrsObjectLoader.java
+++ src/main/java/org/ga4gh/drs/utils/objectloader/HttpsDrsObjectLoader.java
@@ -1,6 +1,10 @@
 package org.ga4gh.drs.utils.objectloader;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -9,15 +13,27 @@ import org.ga4gh.drs.model.Checksum;
 import org.ga4gh.drs.model.ContentsObject;
 import org.ga4gh.drs.model.DrsObject;
 
+import javax.net.ssl.HttpsURLConnection;
+
 public class HttpsDrsObjectLoader extends AbstractDrsObjectLoader {
 
-    public HttpsDrsObjectLoader(String objectId, String objectPath) {
+    private URL url;
+
+    public HttpsDrsObjectLoader(String objectId, String objectPath) throws MalformedURLException {
         super(objectId, objectPath);
+        url = new URL(objectPath);
     }
 
     public boolean exists() {
-        // TODO fill out stub method
-        return false;
+        try {
+            HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+            conn.connect();
+            int responseCode = conn.getResponseCode();
+            // TODO: possibly handling HTTPS -> HTTP redirecting?
+            return 200 <= responseCode && responseCode < 300;
+        } catch (IOException e) {
+            return false;
+        }
     }
 
     public boolean isBundle() {
@@ -40,11 +56,6 @@ public class HttpsDrsObjectLoader extends AbstractDrsObjectLoader {
         return null;
     }
 
-    public URI generateSelfURI() {
-        // TODO fill out stub method
-        return null;
-    }
-
     public DrsObject generateCustomDrsObjectProperties() {
         // TODO fill out stub method
         return null;
@@ -56,8 +67,15 @@ public class HttpsDrsObjectLoader extends AbstractDrsObjectLoader {
     }
 
     public int imputeSize() {
-        // TODO fill out stub method
-        return 0;
+        try {
+            HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+            conn.connect();
+            int responseCode = conn.getResponseCode();
+            // TODO: possibly handling HTTPS -> HTTP redirecting?
+            return conn.getContentLength();
+        } catch (IOException e) {
+            return false;
+        }
     }
 
     public String imputeName() {
